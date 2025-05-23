a1a8f0114cd2fcdb15fe83feeedcfe313ecf2fc7
agent-arace <agen_t_race@outlook.com>
2025-05-23 12:23:33 +0800

    add bugs and features

--- pages/attackanddefense/index.tsx
+++ pages/attackanddefense/index.tsx
@@ -16,32 +16,69 @@ export default function Leaderboard() {
       <Title order={2} className={css.pagetitle}>
         LangChain
       </Title>
+      <Stack bg="var(--mantine-color-body)" gap="sm">
+        <Text fw={700}>Takeaways:</Text>
+        <Text>LangChain's high level of abstraction and encapsulation posed challenges in measuring specific metrics during our experiments.<br />
+        Additionally, LangChain occasionally terminated processes prematurely after reading files from the GAIA dataset, returning the file content directly rather than proceeding with the expected operations.
+        </Text>
+      </Stack>
 
       <Title order={2} className={css.pagetitle}>
         AutoGen
       </Title>
+      <Stack bg="var(--mantine-color-body)" gap="sm">
+        <Text fw={700}>Takeaways:</Text>
+        <Text>Due to the default system prompt being relatively long and containing irrelevant instructions, the RAG workflow may consume unnecessary tokens or produce unexpected errors (e.g., attempting to invoke non-existent tools). Therefore, it is necessary for users to customize the system prompt.</Text>
+      </Stack>
 
 
       <Title order={2} className={css.pagetitle}>
         AgentScope
       </Title>
+      <Stack bg="var(--mantine-color-body)" gap="sm">
+        <Text fw={700}>Takeaways:</Text>
+        <Text>
+        AgentScope’s image and audio processing tools internally rely on OpenAI models, causing their execution time to partially overlap with that of the LLM itself. This overlap can lead to inflated or inaccurate measurements of LLM processing time. Researchers and practitioners should be mindful of this issue when conducting time-based evaluations involving AgentScope.<br />
+        Meanwhile, AgentScope's vector database module, LlamaIndexKnowledge, is implemented based on the BM25Retriever from the llamaindex library. However, the original implementation relies on an outdated version of llamaindex, and recent updates to the library introduced structural changes that break compatibility with the original import statements.<br />
+        To ensure a consistent environment without modifying the framework’s built-in vector database logic, we resolved the issue by duplicating the LlamaIndexKnowledge module and updating the import paths to match the newer llamaindex version.
+        </Text>
+      </Stack>
 
       <Title order={2} className={css.pagetitle}>
         CrewAI
       </Title>
+      <Stack bg="var(--mantine-color-body)" gap="sm">
+        <Text fw={700}>Takeaways:</Text>
+        <Text>When our MOA invokes a large number of agents (>=12), CrewAI system occasionally fails to call all agents completely during execution as intended. For example, when we request 12 sub-agents to be activated, some queries may only trigger 9 or fewer agents.</Text>
+      </Stack>
 
 
       <Title order={2} className={css.pagetitle}>
         Llamaindex
       </Title>
+      <Stack bg="var(--mantine-color-body)" gap="sm">
+        <Text fw={700}>Takeaways:</Text>
+        <Text>LlamaIndex frequently fails to invoke tools correctly, primarily due to the lack of prompt constraints and insufficient post-processing checks on LLM outputs. Without explicit guidance and validation mechanisms, the LLM often produces outputs that do not conform to the expected dictionary format, resulting in tool invocation failures.</Text>
+      </Stack>
 
       <Title order={2} className={css.pagetitle}>
         Phidata
       </Title>
+      <Stack bg="var(--mantine-color-body)" gap="sm">
+        <Text fw={700}>Takeaways:</Text>
+        <Text>It rarely invokes the code execution tool.</Text>
+      </Stack>
 
       <Title order={2} className={css.pagetitle}>
         PydanticAI
       </Title>
+      <Stack bg="var(--mantine-color-body)" gap="sm">
+        <Text fw={700}>Takeaways:</Text>
+        <Text>
+          Within the Pydantic ReAct framework, we observed multiple simultaneous invocations of the same tool, which may lead to inefficiencies. Additionally, similar to Phidata, the code execution tool was seldom triggered.<br />
+          Furthermore, the MoA implementation in the Pydantic framework is tool-based, and not all three models are invoked for every query.
+        </Text>
+      </Stack>
       {/* <Title order={1} className={css.pagetitle}>
         Attacks
       </Title>
